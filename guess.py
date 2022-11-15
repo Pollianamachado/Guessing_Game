488d4dab57e5341bdfadf2170848642b109deeb3
Polliana Machado <116964474+Pollianamachado@users.noreply.github.com>
2022-11-15 21:12:21 +0100

    def game_over()
    Trying to add a "try again" to this game

--- guess.py
+++ guess.py
@@ -1,7 +1,19 @@
 import random
 name = input("Welcome to the Guessing Game! What's your name: ")
 
-         
+def game_over():
+    again= input("Do you want to play again? ")
+    attempt= 0
+    attempt_limit = 4
+    while again == "yes":
+        try:
+            start_game()
+        except again == int():
+            print("you have to write 'yes' or 'no'")
+            continue
+        else: 
+            break
+
 def start_game():
     guess= print(f"Hello, {name}. You have 4 chances to guess it. Let's go!")
     x= random.randint(1,10)  
@@ -14,13 +26,15 @@ def start_game():
             guess= int(input("Try to guess a number from 1 to 10: "))            
         except ValueError:
             print("Sorry, you have to try a number")
+            attempt_limit += 1
             continue
         if guess > 10 or guess < 1:
-            print("sorry, it have to be from 1 to 10")            
+            print("sorry, it have to be from 1 to 10") 
+            attempt_limit += 1           
             continue
         if attempt_limit == 0 and guess != x:
             print("Game over")
-            break         
+            game_over()        
         if guess > x: 
             print("its lower")            
             continue
@@ -29,11 +43,12 @@ def start_game():
             continue   
         else:
             print(f"congratulations, {name}. You guessed it after {attempt} attempt(s)")   
-            break
+            game_over()
         
             
 start_game()
-#add a next chance
+game_over()
+
 
 
 
